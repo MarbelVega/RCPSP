6496dc8e499035469be493ad97279f1492d6cb55
Ravag3r <klich.marc@gmail.com>
2016-05-12 14:48:00 +0200

    Threadpool

--- src/Test.java
+++ src/Test.java
@@ -1,121 +1,50 @@
-
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Test {
 
-	private static int startFileNumber = 1;
-	private static int endFileNumber = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws Exception {
-		
-		double averageDeviation = 0;
+	private static int startFileNumber = 21;	//Start with this file-Nr
+	private static int endFileNumber = 30;		//End with this file-Nr
+	private static int Threadpoolsize = 5;		//Number of parallel tasks
+    private static int populationCap = 5;		//keep n Solutions for each fitness-value
+    private static int numberOfCycles = 50000;	//Target cycle-Number
+    private static int numberOfMutations = 100; //Number of Child mutations
+ 
+    public static void main(String[] args) {
+    	
 		int numberOfSolutions = 0;
 		
 		File folder = new File("instances");
 		File[] listOfFiles = folder.listFiles();
-		
-	    FileWriter fw = new FileWriter("ausgabe.csv",true);
-	    BufferedWriter bw = new BufferedWriter(fw);
 	    
-//		Iterate over all files in the folder
-		for (File file : listOfFiles) {
+        ExecutorService executor = Executors.newFixedThreadPool(Threadpoolsize);
+        
+	    for (File file : listOfFiles) {
 			numberOfSolutions++;
 //			Calculate solution for problem instance
 			if (numberOfSolutions >= startFileNumber && numberOfSolutions <= endFileNumber) {
-				System.out.println("Starting file "+file.getPath());
-				Solution solution = Test.processOneInstance(file.getPath());	
-				String output = "File_name;"+file.getPath()+";FileNr;"+numberOfSolutions+";Cycles;"+solution.getCycles()+";PopulationSize;"+solution.getPopulation()+";Fitness;" + solution.getFitness() + ";Critical_path;"+solution.getCriticalPath() + ";Deviation;"+solution.getDeviation() + ";UID;"+solution.getFittestIndividualUID();
-				System.out.println(output+"\n");
-			   
-				bw.write(output+"\n");
-				bw.flush();
-				
-				averageDeviation = averageDeviation + solution.getDeviation();
+				Runnable worker = new WorkerThread(file.getPath(), numberOfSolutions, populationCap, numberOfCycles, numberOfMutations);
+				executor.execute(worker);	
 			}
-
-			
 			
 		}
-		bw.close();
-		
-		System.out.println((double)averageDeviation / numberOfSolutions);
-	}
-
-	private static Solution processOneInstance(String fileName) throws Exception {
-		//////////////////////////////////////////////////////////////////////////////
-		// 1) READ INSTANCE (http://www.om-db.wi.tum.de/psplib/)
-		Job[] jobs = Job.read(new File(fileName));// best makespan=112
-		Resource[] res = Resource.read(new File(fileName));
-		Solution solution = new Solution();
-		Resource[] criticalPathRessouce = Resource.read(new File(fileName));	
-		Resource.maximizeResources(criticalPathRessouce);		
-		
-		for (int i = 0; i < jobs.length; i++) {
-			jobs[i].calculatePredecessors(jobs);
-		}
-		
-		//Calculating critical Path
-		Individual criticalPathIndividual = new Individual();
-		criticalPathIndividual.initializeJobList(jobs);
-		criticalPathIndividual.decodeJobList(jobs, criticalPathRessouce);
-		solution.setCriticalPathFitness(criticalPathIndividual.getFitness());
-
-		//////////////////////////////////////////////////////////////////////////////
-		// 2) INITIALIZE BEST INDIVIDUAL S
-		Individual firstMother = new Individual();// generate an individual
-		firstMother.initializeJobList(jobs); // initialize genotype of the individual
-		firstMother.decodeJobList(jobs, res); // decoding of the individual
-		//System.out.println("Fitness start point (=makespan): " + firstMother.getFitness());
-
-		Population population = new Population();
-		population.setPopulationCapPerFitnessLevel(5); // keep only 5 individuals per fitness-level
-		population.addIndividual(firstMother);
-		Individual firstFather = new Individual();// generate an individual
-		firstFather.reproduce(firstMother);
-		
-		boolean isNewParent = false;
-		while (isNewParent == false) {
-			for (int i = 0; i < 1000; i++) {
-				firstFather.mutate(jobs);	
+	    
+	    executor.shutdown();
+	    
+        while (!executor.isTerminated()) {
+	        try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			firstFather.decodeJobList(jobs, res);
-			isNewParent = population.addIndividual(firstFather);
-		}
-		
-		Evolution evolution = new Evolution();
-
-		//////////////////////////////////////////////////////////////////////////////
-		// 3) GENERATE NEW INDIVIDUALS BY MUTATION
-		while (population.getCycles() < (50000 - 1) && solution.hasFoundBestSolution() == false) {
-			ArrayList<Individual> pickedParents = evolution.getRankedIndividuals(population); //random pick of two but weighed with fitness //Marc
-			ArrayList<Individual> crossoveredChildren = evolution.fixedCrossover(pickedParents.get(0), pickedParents.get(1)); //crossover the joblists of them //Patrick
-			ArrayList<Individual> crossoveredAndMutatedChildren = evolution.mutate(jobs, crossoveredChildren,100); //take it from individual and put into evolution //Georg
-
-			for (Individual child : crossoveredAndMutatedChildren) {
-				child.decodeJobList(jobs, res);
-				population.addIndividual(child);
-			}
-			
-			if (population.getFittest().getFitness() == criticalPathIndividual.getFitness())
-			{
-				solution.setFoundBestSolution(true);
-			}
-			
-			if(population.getCycles() % 5000 == 0 || solution.hasFoundBestSolution() == true){
-				System.out.println("Cycles: " + population.getCycles() + " - Population: " + population.getPopulationSize() + " - Fittest Individual: " + population.getFittest().getFitness() + " of " + criticalPathIndividual.getFitness());
-			}
-		}
-		Individual bestIndividual = population.getFittest();
-		solution.setFittestIndividualUID(bestIndividual.getUniqueOrderId());
-		solution.setFitness(bestIndividual.getFitness());
-		solution.setCycles(population.getCycles());
-		solution.setPopulation(population.getPopulationSize());
-		return solution;
-	}
+        }
+        
+        System.out.println("Finished all threads");
+    	
+    }
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// next methods are only for checking the data, which has been read from the
